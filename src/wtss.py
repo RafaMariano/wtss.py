e7a0ff62936a301a6191b035a5313f915c3ea8d0
mzaglia <mzaglia@gmail.com>
2016-02-24 13:42:10 -0300

    adding usage for older wtss server

--- src/wtss.py
+++ src/wtss.py
@@ -22,21 +22,50 @@
 import json
 import urllib2
 
+# class wtss:
+#
+#     def __init__(self, host):
+#         self.host = host
+#
+#     def list_coverages(self):
+#         return self.request("%s/wtss/list_coverages" % self.host)
+#
+#
+#     def describe_coverage(self, cv_name):
+#         return self.request("%s/wtss/describe_coverage?name=%s" % (self.host, cv_name))
+#
+#     def time_series(self, cv_name, attributes, latitude, longitude, start_date, end_date):
+#         query_str = "%s/wtss/time_series?coverage=%s&attributes=%s&latitude=%f&longitude=%f" % (self.host, cv_name, ",".join(attributes), latitude, longitude)
+#
+#         if start_date and end_date:
+#             query_str += "&start=%s&end=%s" % (start_date, end_date)
+#
+#         return self.request(query_str)
+#
+#     def request(self, uri):
+#
+#         resource = urllib2.urlopen(uri)
+#
+#         doc = resource.read()
+#
+#         print doc
+#
+#         return json.loads(doc)
+
 class wtss:
 
     def __init__(self, host):
         self.host = host
 
     def list_coverages(self):
-        return self.request("%s/wtss/list_coverages" % self.host)
+        return self.request("%s/mds/product_list?output_format=json" % self.host)
 
 
     def describe_coverage(self, cv_name):
-        return self.request("%s/wtss/describe_coverage?name=%s" % (self.host, cv_name))
+        return self.request("%s/mds/dataset_list?product=%s&output_format=json" % (self.host, cv_name))
 
     def time_series(self, cv_name, attributes, latitude, longitude, start_date, end_date):
-        query_str = "%s/wtss/time_series?coverage=%s&attributes=%s&latitude=%f&longitude=%f" % (self.host, cv_name, ",".join(attributes), latitude, longitude)
-
+        query_str = "%s/mds/query?product=%s&datasets=%s&latitude=%f&longitude=%f&output_format=json" % (self.host, cv_name, ",".join(attributes), latitude, longitude)
         if start_date and end_date:
             query_str += "&start=%s&end=%s" % (start_date, end_date)
 
@@ -52,13 +81,20 @@ class wtss:
 
         return json.loads(doc)
 
-
 if __name__ == '__main__':
-    w = wtss("http://gribeiro1.dpi.inpe.br:7654")
+    # w = wtss("http://gribeiro1.dpi.inpe.br:7654")
+    #
+    # w.list_coverages()
+    #
+    # w.describe_coverage("mcd43a4")
+    #
+    # w.time_series("mcd43a4", ["b1", "b2"], -12.0, -54.0, "", "")
+
+    w = wtss("http://www.dpi.inpe.br/mds")
 
     w.list_coverages()
 
-    w.describe_coverage("mcd43a4")
+    w.describe_coverage("MCD43A4")
 
-    w.time_series("mcd43a4", ["b1", "b2"], -12.0, -54.0, "", "")
+    w.time_series("MCD43A4", ["b1", "b7"], -12.0, -54.0, "", "")
 
