69dd8c76a88098bfb8222fde40f749e245f4eec4
gqueiroz <gribeiro.mg@gmail.com>
2016-09-17 15:44:04 -0300

    packing wtss.py

---
+++ src/wtss/wtss.py
@@ -0,0 +1,50 @@
+#
+#   Copyright (C) 2014 National Institute For Space Research (INPE) - Brazil.
+#
+#  This file is part of Python Client API for Web Time Series Service.
+#
+#  Web Time Series Service for Python is free software: you can
+#  redistribute it and/or modify it under the terms of the
+#  GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License,
+#  or (at your option) any later version.
+#
+#  Web Time Series Service for Python is distributed in the hope that
+#  it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Web Time Series Service for Python. See LICENSE. If not, write to
+#  e-sensing team at <esensning-team@dpi.inpe.br>.
+#
+
+import json
+import urllib2
+
+class wtss:
+
+    def __init__(self, host):
+        self.host = host
+
+    def list_coverages(self):
+        return self.request("%s/wtss/list_coverages" % self.host)
+
+    def describe_coverage(self, cv_name):
+        return self.request("%s/wtss/describe_coverage?name=%s" % (self.host, cv_name))
+
+    def time_series(self, cv_name, attributes, latitude, longitude, start_date, end_date):
+        query_str = "%s/wtss/time_series?coverage=%s&attributes=%s&latitude=%f&longitude=%f" % (self.host, cv_name, ",".join(attributes), latitude, longitude)
+
+        if start_date and end_date:
+            query_str += "&start=%s&end=%s" % (start_date, end_date)
+
+        return self.request(query_str)
+
+    def request(self, uri):
+
+        resource = urllib2.urlopen(uri)
+
+        doc = resource.read()
+
+        return json.loads(doc)
